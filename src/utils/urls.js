e9370399b41b7a89ed5f5b0c58bab36137ddd5c6
zengweipei <zengwp_youwu@163.com>
2020-09-13 17:01:45 +0800

    url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -5,7 +5,7 @@
 
 const REACT_APP_API_URL = process.env.REACT_APP_API_URL
 
-const REACT_APP_STATIC_URL = 'http://localhost:8090'
+const REACT_APP_STATIC_URL = process.env.REACT_APP_STATIC_URL
 
 export  {
     REACT_APP_API_URL,
